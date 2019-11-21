4f5db5627f2918255fafc36b0d027055f1036fd5
Ben Sommerville <bs@Bens-MacBook-Air.local>
2019-11-21 01:19:59 -0500

    message and user added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_013318) do
+ActiveRecord::Schema.define(version: 2019_11_21_061116) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
